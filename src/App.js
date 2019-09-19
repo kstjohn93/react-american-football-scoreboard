c9e0433c2c41ec9e393f2188a3e65536632550a6
kstjohn93 <50053852+kstjohn93@users.noreply.github.com>
2019-09-18 22:10:15 -0500

    added last button- MVP complete

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import BottomRow from "./BottomRow";
 
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
-  const [ homeScore, newHomeScore] = useState(32);
+  const [homeScore, newHomeScore] = useState(32);
   const [awayScore, newAwayScore] = useState(32);
   console.log(newHomeScore)
 
@@ -35,8 +35,8 @@ function App() {
           <button onClick={() => newHomeScore(homeScore + 3)} className="homeButtons__fieldGoal">Home Field Goal</button>
         </div>
         <div className="awayButtons">
-          <button className="awayButtons__touchdown">Away Touchdown</button>
-          <button className="awayButtons__fieldGoal">Away Field Goal</button>
+        <button onClick={() => newAwayScore(awayScore + 7)} className="awayButtons__touchdown">Away Touchdown</button>
+          <button onClick={() => newAwayScore(awayScore + 3)} className="awayButtons__fieldGoal">Away Field Goal</button>
         </div>
       </section>
     </div>
